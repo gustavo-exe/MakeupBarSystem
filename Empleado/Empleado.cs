35a2bebd3c61353d22d18b41610ff47f002046b7
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2020-03-28 20:44:36 -0600

    Clase empleado contructor y propiedades

---
+++ Empleado/Empleado.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeupBarSystem.Empleado
+{
+    class Empleado
+    {
+        private int idEmpleado;
+        private string usuario;
+        private string password;
+        private string rol;
+
+        public Empleado()
+        {
+            idEmpleado = 0;
+            usuario = "";
+            password = "";
+            rol = "";
+        }
+
+        public Empleado(int id, string user, string pass, string puesto)
+        {
+            id = idEmpleado;
+            user = usuario;
+            pass = password;
+            puesto = rol;
+        }
+
+        public int IdEmpleado
+        {
+            get { return idEmpleado; }
+        }
+
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Rol
+        {
+            get { return rol; }
+            set { rol = value; }
+        }
+
+
+
+    }
+}
