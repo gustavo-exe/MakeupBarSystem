fb6b2bbb5bc7858266b0e99994bd7a2b7b9c342d
wilmergarcia7 <61239560+wilmergarcia7@users.noreply.github.com>
2020-04-13 23:35:04 -0600

    actualizacion de datos
    Se agrego la función de autocompletar y arreglo en combobox

--- Conexion.cs
+++ Conexion.cs
@@ -21,12 +21,12 @@ namespace MakeupBarSystem
         public Conexion()
         {
             //Elias
-
+            /*
             bd = "makeupbar";
             servidor = "127.0.0.1";
             usuario = "MakeupBarElias";
             pass = "Y o l o Y o l o 1";
-
+            -*
             // yovany
             /* 
              bd = "makeupbar";
@@ -47,11 +47,11 @@ namespace MakeupBarSystem
             pass = "987654321";*/
 
             //W I L M E R 
-            /*bd = "makeuppruebas";
+            bd = "makeupbar";
             servidor = "127.0.0.1";
             usuario = "wilmer";
             pass = "Rasengan 1";
-            */
+            
 
             //Dina
             //bd = "makeupbar";
